45153b058619183a0cf3f196f6950216e551fe54
jaepani <dev_jhkim@kakao.com>
2020-11-15 14:00:12 +0900

    - 백엔드 작업 수행 - 비동기 처리 때문에 request, xml-js 실행 오류

---
+++ backend/reqFunc.js
@@ -0,0 +1,32 @@
+const convert = require('xml-js');
+const request = require('request');
+
+const AUTH_KEY = require('./security');
+
+// 코로나19 감염현황
+const HOST_INFCTION_STATUS = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson';
+// 코로나19 시/도 발생 현황
+const HOST_REGION = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson';
+// 코로나19 연령별/성별 감염 현황
+const HOST_AGE_GENDER = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/ getCovid19GenAgeCaseInfJson';
+// 코로나19 해외발생현황
+const HOST_FOREIGN = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19NatInfStateJson';
+
+const requestUrl = `${HOST_INFCTION_STATUS}?serviceKey=${AUTH_KEY}&numOfRows=10&startCreateDt=20201101&endCreateDt=20201113`;
+
+const changeJson = (req, res, next) => {
+    console.log('changeJson func');
+    request.get(requestUrl, (err, res, body) => {
+        if (err) console.log('Error!!!', err);
+        else {
+            if (res.statusCode === 200) {
+                const result = body;
+                const xmlToJson = convert.xml2json(result, {compact: true, spaces: 4});
+                return xmlToJson;
+            }
+        }
+    })
+    next();
+};
+
+module.exports = changeJson;
